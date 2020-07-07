6465e9475cb08c6fc6e47857fe14dbf0d0a952e6
smoofy24 <splavecl@gmail.com>
2020-07-07 13:52:51 +0200

    Complete queen_attack

---
+++ erlang/queen-attack/_build/test/lib/queen_attack/ebin/queen_attack.app
@@ -0,0 +1,7 @@
+{application,queen_attack,
+             [{description,"exercism.io - queen-attack"},
+              {vsn,"0.0.1"},
+              {modules,[queen_attack]},
+              {registered,[]},
+              {applications,[kernel,stdlib]},
+              {env,[]}]}.
--- erlang/queen-attack/src/queen_attack.erl
+++ erlang/queen-attack/src/queen_attack.erl
@@ -3,4 +3,23 @@
 -export([can_attack/2]).
 
 
-can_attack(_WhiteQueen, _BlackQueen) -> undefined.
+can_attack(_WhiteQueen, _BlackQueen) ->
+    %%
+    %% Grab the values from the input
+    %%
+    {X1, Y1} = _WhiteQueen, 
+    {X2, Y2} = _BlackQueen,
+
+    D = abs(X2 - X1) - abs(Y2 - Y1), 
+
+    if 
+        X1 == X2 ->
+            true;
+        Y1 == Y2 ->
+            true;
+        D  == 0  ->
+            true;
+        true ->
+            false
+    end.
+    
